ee5d89a7844056036566517c1ad572d6cfd8c8d1
darshansutariya <59061390+darshansutariya@users.noreply.github.com>
2024-04-30 09:23:55 +0530

    Create APIService.kt

---
+++ APIService.kt
@@ -0,0 +1,30 @@
+package com.app.ticketwingsuser.apiCalling
+
+import com.google.gson.JsonObject
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.core.Single
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
+import retrofit2.http.Body
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.Headers
+import retrofit2.http.Multipart
+import retrofit2.http.POST
+import retrofit2.http.Part
+
+interface APIService {
+
+    @POST("auth/signup")
+    fun signup(@Body signUpRequest: SignUpRequest): Observable<SignUpResponse>
+
+
+    // MediaUpload
+    @Multipart
+    @POST("common/mediaUpload")
+    fun mediaUpload(
+        @Part("langType") fullName: RequestBody?,
+        @Part files: MultipartBody.Part,
+    ): Observable<MediaUploadResponse>
+
+}
